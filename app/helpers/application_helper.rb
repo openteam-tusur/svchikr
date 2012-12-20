c288d386d2aa2e6c3c3cf933a80c45ada62c8bd6
Evgeny Lapin <lev@openteam.ru>
2012-12-20 16:59:59 +0700

    add sitemap template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,4 +49,15 @@ module ApplicationHelper
     content_tag :p, content.squish.gsub(/;$/, '').html_safe if content.present?
   end
 
+  def render_navigation(hash)
+    return '' if hash.nil? || hash.empty?
+    content_tag :ul do
+      hash.map do |key, value|
+        content_tag :li, :class => value['selected'] ? 'selected' : nil do
+          link_to(value['title'], value['path']) + render_navigation(value['children'])
+        end
+      end.join("\n").html_safe
+    end
+  end
+
 end
